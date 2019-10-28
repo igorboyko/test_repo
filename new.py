20711844314e1d3d09775363226cd40811a9cbeb
Igor Boyko <igor.a.boyko@gmail.com>
2019-10-28 15:41:41 +0300

    first commit

--- new.py
+++ new.py
@@ -1,3 +1,13 @@
-print('Hello,Igor !')
-print('i find work!!!')
+#create dictionary
+nw_dict={'B':'a','A':'b'}
+q=sorted(nw_dict)
+print(q)
+print(nw_dict)
+print(nw_dict['A'])
+if True=='C' in nw_dict:
+    print('In array C')
+else:
+    print('Not in dict')
+print(nw_dict.keys())
 
+print(nw_dict.values())
